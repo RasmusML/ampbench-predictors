34dc0d21e786c11f56bbc5a39a02c5b8884ed601
RasmusML <rasmusml@hotmail.dk>
2026-02-16 22:17:52 +0100

    Fix path

--- omegamp/constants.py
+++ omegamp/constants.py
@@ -1,32 +1,24 @@
-MODEL_DIR = "models/"
+from pathlib import Path
+
+REPO_PATH = Path(__file__).resolve().parents[1]
+
+MODEL_DIR = REPO_PATH / "models"
 
 CLASSIFIER_MODELS = {
-    "broad-classifier": MODEL_DIR + "broad-classifier.json",
-    "species-acinetobacterbaumannii-classifier": MODEL_DIR
-    + "species-acinetobacterbaumannii-classifier.json",
-    "species-escherichiacoli-classifier": MODEL_DIR
-    + "species-escherichiacoli-classifier.json",
-    "species-klebsiellapneumoniae-classifier": MODEL_DIR
-    + "species-klebsiellapneumoniae-classifier.json",
-    "species-pseudomonasaeruginosa-classifier": MODEL_DIR
-    + "species-pseudomonasaeruginosa-classifier.json",
-    "species-staphylococcusaureus-classifier": MODEL_DIR
-    + "species-staphylococcusaureus-classifier.json",
-    "strains-acinetobacterbaumanniiatcc19606-classifier": MODEL_DIR
-    + "strains-acinetobacterbaumanniiatcc19606-classifier.json",
-    "strains-escherichiacoliatcc25922-classifier": MODEL_DIR
-    + "strains-escherichiacoliatcc25922-classifier.json",
-    "strains-klebsiellapneumoniaeatcc700603-classifier": MODEL_DIR
-    + "strains-klebsiellapneumoniaeatcc700603-classifier.json",
-    "strains-pseudomonasaeruginosaatcc27853-classifier": MODEL_DIR
-    + "strains-pseudomonasaeruginosaatcc27853-classifier.json",
-    "strains-staphylococcusaureusatcc25923-classifier": MODEL_DIR
-    + "strains-staphylococcusaureusatcc25923-classifier.json",
-    "strains-staphylococcusaureusatcc33591-classifier": MODEL_DIR
-    + "strains-staphylococcusaureusatcc33591-classifier.json",
-    "strains-staphylococcusaureusatcc43300-classifier": MODEL_DIR
-    + "strains-staphylococcusaureusatcc43300-classifier.json",
-    "hemolytic-classifier": MODEL_DIR + "hemolytic-classifier.json",
-}
+    "broad-classifier": MODEL_DIR / "broad-classifier.json",
+    "species-acinetobacterbaumannii-classifier": MODEL_DIR / "species-acinetobacterbaumannii-classifier.json",
+    "species-escherichiacoli-classifier": MODEL_DIR / "species-escherichiacoli-classifier.json",
+    "species-klebsiellapneumoniae-classifier": MODEL_DIR / "species-klebsiellapneumoniae-classifier.json",
+    "species-pseudomonasaeruginosa-classifier": MODEL_DIR / "species-pseudomonasaeruginosa-classifier.json",
+    "species-staphylococcusaureus-classifier": MODEL_DIR / "species-staphylococcusaureus-classifier.json",
+    "strains-acinetobacterbaumanniiatcc19606-classifier": MODEL_DIR / "strains-acinetobacterbaumanniiatcc19606-classifier.json",
+    "strains-escherichiacoliatcc25922-classifier": MODEL_DIR / "strains-escherichiacoliatcc25922-classifier.json",
+    "strains-klebsiellapneumoniaeatcc700603-classifier": MODEL_DIR / "strains-klebsiellapneumoniaeatcc700603-classifier.json",
+    "strains-pseudomonasaeruginosaatcc27853-classifier": MODEL_DIR / "strains-pseudomonasaeruginosaatcc27853-classifier.json",
+    "strains-staphylococcusaureusatcc25923-classifier": MODEL_DIR / "strains-staphylococcusaureusatcc25923-classifier.json",
+    "strains-staphylococcusaureusatcc33591-classifier": MODEL_DIR / "strains-staphylococcusaureusatcc33591-classifier.json",
+    "strains-staphylococcusaureusatcc43300-classifier": MODEL_DIR / "strains-staphylococcusaureusatcc43300-classifier.json",
+    "hemolytic-classifier": MODEL_DIR / "hemolytic-classifier.json",
+}  # fmt: off
 
 AMINO_ACIDS = list("ACDEFGHIKLMNPQRSTVWY")
